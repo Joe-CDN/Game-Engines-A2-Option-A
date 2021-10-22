43aff77872e49c0caad0c6d1570594a36bc12850
Joe-CDN <35740892+Joe-CDN@users.noreply.github.com>
2021-10-22 17:04:41 -0400

    w/command and observer

---
+++ Assets/Scripts/Factory Pattern/Obstacle.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace simpleFactory
+{
+    public abstract class Obstacle
+    {
+        public abstract void Process();
+    }
+
+    public class enemyObstacle
+    {
+        
+    }
+}
